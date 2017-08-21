922950cf15d7c282fa637df05945028fc8f54320
pjm605 <pjm605@gmail.com>
2017-08-21 19:20:00 -0400

    added deadline/refund

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -6,6 +6,7 @@ contract Remittance {
     struct LockInfo {
         uint amount;
         bytes32 pw1pw2Hash;
+        uint deadline;
     }
     
     mapping(address => LockInfo) public lockInfos;
@@ -15,7 +16,7 @@ contract Remittance {
         owner = msg.sender;
     }
     
-    function registerLockInfo(bytes32 _pw1Hash, bytes32 _pw2Hash)
+    function registerLockInfo(bytes32 _pw1Hash, bytes32 _pw2Hash, uint deadlineDuration)
         public
         payable
         returns (bool)
@@ -25,6 +26,8 @@ contract Remittance {
         
         lockInfos[msg.sender].pw1pw2Hash = keccak256(_pw1Hash, _pw2Hash);
         lockInfos[msg.sender].amount = msg.value;
+        lockInfos[msg.sender].deadline = block.number + deadlineDuration;
+        
         return true;
     }
     
@@ -37,21 +40,27 @@ contract Remittance {
         bytes32 combinedHash = keccak256(_pw1Hash, _pw2Hash);
 
         require(lockInfos[payer].pw1pw2Hash == combinedHash);
+
+        if (!msg.sender.send(lockInfos[payer].amount)) throw;
+        lockInfos[payer].amount = 0;
         
-        if (!msg.sender.send(lockInfos[payer].amount)) {
-            // todo handle error in this case
-        }
-        else {
-            // clear the amount (reset)
-            lockInfos[payer].amount = 0;
-        }
+        return true;
+    }
+    
+    function refundExpiredRemittance () returns (bool) {
+        require (lockInfos[msg.sender].amount > 0);
+        require (lockInfos[msg.sender].deadline < block.number);
+ 
+        uint amountToRefund = lockInfos[msg.sender].amount;
+        
+        msg.sender.transfer(amountToRefund);
+        lockInfos[msg.sender].amount = 0;
+        
         return true;
     }
     
     function getHash(string pw) public constant returns(bytes32) {
         return keccak256(pw);
-        
-        
     }
     
     
